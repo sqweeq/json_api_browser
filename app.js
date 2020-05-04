74f7c0e4aedbf88c345a1b3e3fa09a0ff322c46e
sqweeq <tsai8@hotmail.com>
2020-05-05 00:37:11 +1200

    added rules

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const remove_class_on_scroll = () =>
   header.classList.remove("scroll-top-effect");
 
 // add the class or remove depending on scroll position
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   scrollpos = window.scrollY;
 
   if (scrollpos >= header_height) {
@@ -16,5 +16,53 @@ window.addEventListener("scroll", function() {
     remove_class_on_scroll();
   }
 
-  console.log(scrollpos);
+  // console.log(scrollpos);
 });
+// get all rules
+const req = new XMLHttpRequest();
+req.open("GET", "https://us-west-2.cloudconformity.com/v1/services", true);
+req.onload = function () {
+  const data = JSON.parse(this.response);
+  // map over data, compare and match ID's, find and return
+  const Rules = data.data.map((item) => ({
+    Category: item.id,
+    Index: item.relationships.rules.data.map(
+      (x) =>
+        data.included.filter((k) => k.id == x.id).find((b) => b.attributes)
+          .attributes
+    ),
+  }));
+
+  console.log(Rules);
+
+  // render rules in list onto page
+  const allRules = document.querySelector(".all-rules");
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "service-list");
+
+  Rules.forEach((item) => {
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+
+    const a = document.createElement("a");
+
+    li.setAttribute("class", "service-link");
+    a.textContent = item.Category;
+    ul.appendChild(li);
+    li.appendChild(h3);
+    h3.appendChild(a);
+
+    item.Index.forEach((z) => {
+      const h6 = document.createElement("h6");
+      h6.textContent = z.title;
+      li.appendChild(h6);
+      const p = document.createElement("p");
+      p.textContent = z.description;
+      p.setAttribute("class", "text-secondary");
+      li.appendChild(p);
+    });
+    allRules.appendChild(ul);
+  });
+};
+
+req.send();
